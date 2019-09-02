44c6d7e52e1f78403ec1d24d5a858eecb3184509
Raphael <raphsquat@gmail.com>
2019-09-02 04:32:44 +0200

    Homepage new design

--- App/Frontend/Modules/News/Views/index.php
+++ App/Frontend/Modules/News/Views/index.php
@@ -4,10 +4,16 @@
 	foreach ($listeNews as $news){
 	?>
 				<section>
-					<h2><a href="news-<?= $news['id'] ?>.html"><?= $news['titre'] ?></a></h2>
-					<p><?= $news['dateAjout']->format('d.m.Y') ?></p>
-					<p><?= (nl2br($news['contenu']))?></p>
-					<a href="news-<?= $news['id'] ?>.html"><h4>Lire</h4></a>
+					<div class="articleInfos">
+						<h2><a href="news-<?= $news['id'] ?>.html"><?= $news['titre'] ?></a></h2>
+						<p><?= $news['dateAjout']->format('d.m.Y') ?></p>
+						
+					</div>
+					<div class="articlePreview">
+						<p><?= (nl2br($news['contenu']))?></p>
+						<a href="news-<?= $news['id'] ?>.html"><h4>Lire</h4></a>
+					</div>
+									
 				</section>
 	<?php } ?>
 </div>
