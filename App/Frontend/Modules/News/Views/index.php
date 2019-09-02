2fa8137596d1a7c33b678870bb17b5a3b8c7732f
Raphael <raphsquat@gmail.com>
2019-09-02 02:21:12 +0200

    Total rebranding base

--- App/Frontend/Modules/News/Views/index.php
+++ App/Frontend/Modules/News/Views/index.php
@@ -7,7 +7,7 @@
 					<h2><a href="news-<?= $news['id'] ?>.html"><?= $news['titre'] ?></a></h2>
 					<p><?= $news['dateAjout']->format('d.m.Y') ?></p>
 					<p><?= (nl2br($news['contenu']))?></p>
-					<a href="news-<?= $news['id'] ?>.html"><h4 class="button">Lire</h4></a>
+					<a href="news-<?= $news['id'] ?>.html"><h4>Lire</h4></a>
 				</section>
 	<?php } ?>
 </div>
--- App/Frontend/Templates/layout.php
+++ App/Frontend/Templates/layout.php
@@ -6,17 +6,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="/css/style.css">
-    <link href="https://fonts.googleapis.com/css?family=Maven+Pro:400,500,700,900&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Playfair+Display:400,400i,700,700i,900,900i&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css">
-      <script src="https://cdn.tiny.cloud/1/26ywukqer0d0dfv9zm2c0nuvp0szg6glxts0nynvfxm28aui/tinymce/5/tinymce.min.js"></script>
+    <script src="https://cdn.tiny.cloud/1/26ywukqer0d0dfv9zm2c0nuvp0szg6glxts0nynvfxm28aui/tinymce/5/tinymce.min.js"></script>
     <script>tinymce.init({selector:'textarea'});</script>
   </head>
   
   <body>
       <?php include('header.php')?>
       <div id="brand">
-        <h1>Billet simple pour l'Alaska</h1>
-        <h3>Jean Forteroche</h3>
+        <h1><span class="topBrand">Billet simple pour</span>l'Alaska <span class="botBrand">Jean Forteroche</span></h1>
+        
       </div>
     	<main class="container">
           <?= $content ?>
